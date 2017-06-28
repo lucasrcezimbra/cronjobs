2bf3714305b474012031887ad509f3cdc94b5edc
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-06-28 01:03:44 -0300

    Nubank-Spreadsheet: move google spreadsheet credentials to environment variables

--- nubank-spreadsheet/main.py
+++ nubank-spreadsheet/main.py
@@ -1,4 +1,5 @@
 import os
+import json
 from datetime import date, timedelta
 
 import pandas as pd
@@ -12,6 +13,9 @@ def main():
     SPREADSHEET = 'Gastos 2017'
     WORKSHEET = 'NuBank'
     AUTH_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'service_creds.json')
+
+    if not os.path.exists(AUTH_FILE):
+        __create_auth_file(AUTH_FILE)
 
     print('--- Getting NuBank events ---')
     nubank = Nubank(NUBANK_CPF, NUBANK_PASSWORD)
@@ -28,6 +32,23 @@ def main():
     worksheet = gc.open(SPREADSHEET).worksheet_by_title(WORKSHEET)
     values = [list(r) for r in last_events]
     worksheet.insert_rows(1, number=len(values), values=values)
+
+def __create_auth_file(filepath):
+    auth_dict = {
+        'type': config('AUTH_TYPE'),
+        'project_id': config('AUTH_PROJECT_ID'),
+        'private_key_id': config('AUTH_PRIVATE_KEY_ID'),
+        'private_key': config('AUTH_PRIVATE_KEY', cast=lambda v: v.replace('\\n', '\n')),
+        'client_email': config('AUTH_CLIENT_EMAIL'),
+        'client_id': config('AUTH_CLIENT_ID'),
+        'auth_uri': config('AUTH_AUTH_URI'),
+        'token_uri': config('AUTH_TOKEN_URI'),
+        'auth_provider_x509_cert_url': config('AUTH_AUTH_PROVIDER_X509_CERT_URL'),
+        'client_x509_cert_url': config('AUTH_CLIENT_X509_CERT_URL'),
+    }
+    with open(filepath, 'w') as fp:
+        json.dump(auth_dict, fp)
+
 
 def __get_yesterday_events_records(dataframe):
     yesterday = date.today() - timedelta(1)
