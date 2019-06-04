e8f455d6ccd14680403e15387dffde6c90ab2a61
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2019-06-03 22:33:57 -0300

    nubank: add debit events

--- nubank_spreadsheet/__main__.py
+++ nubank_spreadsheet/__main__.py
@@ -12,49 +12,95 @@ from spreadsheets import insert
 def main(initial_date=None):
     if not initial_date:
         initial_date = date.today() - timedelta(1)
-    initial_datetime = datetime.combine(initial_date, datetime.min.time())
-    initial_datetime = initial_datetime.astimezone()
 
-    MONTHS = ['', 'Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+    MONTHS = [
+        '',
+        'Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun',
+        'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+    ]
     NUBANK_CPF = config('NUBANK_CPF')
     NUBANK_PASSWORD = config('NUBANK_PASSWORD')
     SPREADSHEET = 'Gastos {}'.format(date.today().year)
-    WORKSHEET = MONTHS[initial_datetime.month]
+    WORKSHEET = MONTHS[initial_date.month]
 
     print('--- Getting NuBank events ---')
     nubank = Nubank(NUBANK_CPF, NUBANK_PASSWORD, allow_qr_code_auth=True)
-    nubank_events = nubank.get_card_statements()
+    credit_events = nubank.get_card_statements()
+    debit_events = nubank.get_account_statements()
 
     print('--- NuBank events to DataFrame ---')
-    dataframe = __create_dataframe(nubank_events)
-    last_events = __get_events_records_by_date(dataframe, initial_datetime)
+    dataframe = __create_dataframe(credit_events, debit_events, initial_date)
+    last_events = list(dataframe.to_records(index=False))
 
     values = [list(r) for r in last_events]
     insert(SPREADSHEET, WORKSHEET, values)
 
 
-def __get_events_records_by_date(dataframe, date):
-    last_events = dataframe.loc[dataframe['time'] > date]
-    last_events['time'] = last_events['time'].apply(str)
-    return list(last_events.to_records(index=False))
+def __create_dataframe(credit_events, debit_events, date):
+    credit_dataframe = __create_credit_dataframe(credit_events)
+    debit_dataframe = __create_debit_dataframe(debit_events)
 
+    df = pd.concat((credit_dataframe, debit_dataframe))
 
-def __create_dataframe(nubank_events):
-    columns = ['time', 'category', 'description', 'nubank', 'shop', 'shop2', 'parcela', 'amount', 'reembolso', 'total']
-    df = pd.DataFrame(nubank_events, columns=columns)
-    df['time'] = pd.to_datetime(df['time'])
+    df['time'] = df['time'].apply(lambda x: x.date())
+    df = df.loc[df['time'] > date]
+    df.sort_values('time', inplace=True)
+    df['time'] = df['time'].apply(str)
+
+    df.reset_index(inplace=True, drop=True)
+
     df['category'] = df.index + 2
-    df['category'] = df['category'].apply(lambda i: '=VLOOKUP(F{};Categorias!H:I;2;FALSE)'.format(i))
-    df['nubank'] = 'NuBank'
-    df['shop'] = df['description']
+    df['category'] = df['category'].apply(
+        lambda i: '=VLOOKUP(F{};Categorias!H:I;2;FALSE)'.format(i))
+
     df['shop2'] = df.index + 2
-    df['shop2'] = df['shop2'].apply(lambda i: '=VLOOKUP(E{};Categorias!E:F;2;FALSE)'.format(i))
-    df['description'] = None
-    df['parcela'] = ''
-    df['amount'] = df['amount'].apply(int).apply(str).apply(lambda x: "-{},{}".format(x[0:-2], x[-2:]))
-    df['reembolso'] = None
+    df['shop2'] = df['shop2'].apply(
+        lambda i: '=VLOOKUP(E{};Categorias!E:F;2;FALSE)'.format(i))
+
     df['total'] = df.index + 2
     df['total'] = df['total'].apply(lambda i: '=H{}+I{}'.format(i, i))
+
+    return df
+
+
+def __create_credit_dataframe(events):
+    columns = [
+        'time', 'category', 'description', 'nubank', 'shop', 'shop2',
+        'parcela', 'amount', 'reembolso', 'total',
+    ]
+    df = pd.DataFrame(events, columns=columns)
+    df['time'] = pd.to_datetime(df['time'])
+    df['nubank'] = 'NuBank'
+    df['shop'] = df['description']
+    df['description'] = None
+    df['parcela'] = ''
+    df['amount'] = df['amount'].apply(int).apply(lambda x: (x / 100) * -1)
+    df['reembolso'] = None
+    return df
+
+
+def __create_debit_dataframe(events):
+    columns = [
+        '__typename', 'postDate', 'category', 'title', 'nubank', 'shop',
+        'shop2', 'destinationAccount', 'originAccount', 'parcela', 'amount',
+        'reembolso', 'total'
+    ]
+    df = pd.DataFrame(events, columns=columns)
+    df.rename(columns={'title': 'description', 'postDate': 'time'},
+              inplace=True)
+    df['time'] = pd.to_datetime(df['time'])
+    df['category'] = None
+    df['nubank'] = 'NuConta'
+    df['destinationAccount'] = df['destinationAccount'].apply(pd.Series)['name']
+    df['originAccount'] = df['originAccount'].apply(pd.Series)['name']
+    df.fillna('', inplace=True)
+    df['shop'] = df["destinationAccount"] + df["originAccount"]
+    df['parcela'] = None
+    df.loc[df['__typename'] == 'TransferOutEvent', 'amount'] = df['amount'].apply(lambda x: x * -1)
+    df.loc[df['__typename'] == 'BarcodePaymentEvent', 'amount'] = df['amount'].apply(lambda x: x * -1)
+    del df['destinationAccount']
+    del df['originAccount']
+    del df['__typename']
     return df
 
 
