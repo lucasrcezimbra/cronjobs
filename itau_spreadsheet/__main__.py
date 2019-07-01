e748ca815890dd365a7e901a8e6f11079114900d
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2019-07-01 00:53:16 -0300

    itau: first blood

---
+++ itau_spreadsheet/__main__.py
@@ -0,0 +1,102 @@
+import sys
+from datetime import date, datetime, timedelta
+
+import pandas
+from decouple import config
+from pyitau import Itau
+
+from utils.log import logger
+from spreadsheets import insert
+
+
+def main(initial_date=None):
+    if not initial_date:
+        initial_date = date.today() - timedelta(1)
+
+    MONTHS = [
+        '',
+        'Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun',
+        'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+    ]
+    AGENCY = config('ITAU_AGENCY')
+    ACCOUNT = config('ITAU_ACCOUNT')
+    ACCOUNT_DIGIT = config('ITAU_ACCOUNT_DIGIT')
+    PASSWORD = config('ITAU_PASSWORD')
+    SPREADSHEET = 'Gastos {}'.format(date.today().year)
+    WORKSHEET = MONTHS[initial_date.month]
+
+    print('--- Getting Itaú events ---')
+    itau = Itau(AGENCY, ACCOUNT, ACCOUNT_DIGIT, PASSWORD)
+    itau.authenticate()
+    events = itau.get_statements()['lancamentos']
+
+    print('--- Itaú events to DataFrame ---')
+    dataframe = __create_dataframe(events, initial_date)
+    last_events = list(dataframe.to_records(index=False))
+
+    values = [list(r) for r in last_events]
+    insert(SPREADSHEET, WORKSHEET, values)
+
+
+def __create_dataframe(events, date):
+    df = __create_account_dataframe(events)
+
+    df['date'] = df['date'].apply(lambda x: x.date())
+    df = df.loc[df['date'] > date]
+    df.sort_values('date', inplace=True)
+    df['date'] = df['date'].apply(str)
+
+    df.reset_index(inplace=True, drop=True)
+
+    df['category'] = df.index + 2
+    df['category'] = df['category'].apply(
+        lambda i: '=VLOOKUP(F{};Categorias!H:I;2;FALSE)'.format(i))
+
+    df['location2'] = df.index + 2
+    df['location2'] = df['location2'].apply(
+        lambda i: '=VLOOKUP(E{};Categorias!E:F;2;FALSE)'.format(i))
+
+    df['total'] = df.index + 2
+    df['total'] = df['total'].apply(lambda i: '=H{}+I{}'.format(i, i))
+
+    return df
+
+
+def __create_account_dataframe(events):
+    columns = [
+        'dataLancamento', 'category', 'descricaoLancamento', 'itau',
+        'location', 'location2', 'parcela', 'valorLancamento', 'reembolso',
+        'total', 'ePositivo',
+    ]
+    columns_mapper = {
+        'dataLancamento': 'date',
+        'descricaoLancamento': 'description',
+        'valorLancamento': 'amount'
+    }
+    df = pandas.DataFrame(events, columns=columns)
+    df.rename(columns=columns_mapper, inplace=True)
+    df['date'] = pandas.to_datetime(df['date'])
+    df['category'] = None
+    df['itau'] = 'Itaú'
+    df['location'] = df['description']
+    df['location2'] = None
+    df['parcela'] = None
+    df['amount'] = df['amount'].apply(lambda x: x.replace('.', ''))
+    df['amount'] = df['amount'].apply(lambda x: x.replace(',', '.'))
+    df['amount'] = df['amount'].apply(float)
+    df.loc[df['ePositivo'] == False, 'amount'] = df['amount'].apply(lambda x: x * -1)
+    df['reembolso'] = None
+    df['total'] = None
+    del df['ePositivo']
+    return df
+
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) > 1:
+            initial_date = datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
+            main(initial_date)
+        else:
+            main()
+    except Exception as e:
+        logger.exception(e)
