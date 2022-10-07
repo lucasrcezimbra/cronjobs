10d46b906e8c29cfc184b76412b9abb370a63de4
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-10-07 18:33:43 -0300

    agilize_prolabore: Add Google Drive upload

--- agilize_prolabore/__main__.py
+++ agilize_prolabore/__main__.py
@@ -1,8 +1,9 @@
 import sys
-from pathlib import Path
 
 from agilize import Agilize, Competence
 from decouple import config
+
+from storage.google import drive
 
 
 def main(competence):
@@ -12,10 +13,11 @@ def main(competence):
     competence = Competence.from_data(competence)
     prolabore = company.prolabores.get(competence)
 
-    filepath = Path.home() / 'Downloads' / f'{competence}-Lucas Rangel Cezimbra.pdf'
-
-    with open(filepath, 'wb') as f:
-        f.write(prolabore.download())
+    drive.upload(
+        file=prolabore.download(),
+        filename=f'{competence}-Lucas Rangel Cezimbra.pdf',
+        folder=drive.PROLABORE_FOLDER,
+    )
 
 
 if __name__ == '__main__':
