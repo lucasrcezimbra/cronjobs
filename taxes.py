4cef7476ed7aab0263f6e7e4ba30c2d7e3c30e26
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-11-28 18:32:58 -0300

    taxes: print barcode

--- taxes.py
+++ taxes.py
@@ -1,9 +1,30 @@
+import re
 import sys
+from io import BytesIO
 
 from agilize import Agilize, Competence
 from decouple import config
+from PyPDF2 import PdfReader
 
 from storage.google import drive
+
+# ~1. Subir boletos para o drive~
+# 2. Pagar
+# 3. Subir comprovantes para o Drive
+# 4. Subir comprovantes para a Agilize
+# 4. Atualizar contabilidade
+
+
+def barcode_from_pdf(file):
+    reader = PdfReader(BytesIO(file))
+    text = reader.pages[0].extract_text()
+    raw_barcode = re.search(r'[0-9 ]{48,100}', text).group()
+    barcode = raw_barcode.replace(' ', '')
+
+    if len(barcode) != 48:
+        raise ValueError("Barcode doesn't have 48 numbers")
+
+    return barcode
 
 
 def main(competence):
@@ -13,8 +34,14 @@ def main(competence):
     competence = Competence.from_data(competence)
 
     for tax in company.taxes.filter(competence):
+        file = tax.download()
+
+        if tax.abbreviation in ('DAS', 'DCTFWeb'):
+            # 2. TODO: inter.pay_barcode
+            print(tax.abbreviation, barcode_from_pdf(file))
+
         drive.upload(
-            file=tax.download(),
+            file=file,
             filename=f'{tax.abbreviation}_{competence}.pdf',
             folder=drive.TAXES_FOLDER,
         )
