b541c4bc889080040abee3b7ea926dae74f6572b
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2023-08-29 08:24:06 -0300

    Move nubank_spreadsheet to gastos.nubank

---
+++ cronjobs/gastos/nubank/__main__.py
@@ -0,0 +1,48 @@
+import sys
+from datetime import date
+
+from decouple import config
+from pynubank import Nubank
+
+from cronjobs.gastos import sheet
+from cronjobs.gastos.nubank import credit_card
+from cronjobs.logs import get_logger
+
+logger = get_logger(__name__)
+
+
+def main(date_):
+    year_month = date_.strftime('%Y-%m')
+
+    nubank = Nubank()
+    logger.info('Authenticating with Nubank')
+    nubank.authenticate_with_cert(
+        cpf=config('NUBANK_CPF'),
+        password=config('NUBANK_PASSWORD'),
+        cert_path=config('NUBANK_CERT_PATH'),
+    )
+
+    logger.info('Requesting bills')
+    bills_data = nubank.get_bills()
+    logger.info('Requesting card statements')
+    card_statements_data = nubank.get_card_statements()
+
+    bill_data = credit_card.get_bill_data(bills_data, year_month)
+    bill_detail_data = nubank.get_bill_details(bill_data)
+
+    rows_credit_card = credit_card.data_to_rows(
+        bill_detail_data=bill_detail_data,
+        card_statements_data=card_statements_data,
+        nubank=nubank,
+        year_month=year_month,
+    )
+
+    logger.info(f'Inserting {len(rows_credit_card)} rows into the spreadsheet')
+    sheet.insert(rows_credit_card, date_)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python -m cronjobs.gastos.nubank <year>-<month>-<day>')
+
+    main(date.fromisoformat(sys.argv[1]))
---
+++ cronjobs/gastos/nubank/credit_card.py
@@ -0,0 +1,83 @@
+from datetime import date, datetime
+from decimal import Decimal
+from typing import List, Optional
+
+from attrs import define
+from cattrs import Converter
+
+from cronjobs.gastos.sheet import Row
+from cronjobs.logs import get_logger
+
+logger = get_logger(__name__)
+
+converter = Converter()
+converter.register_structure_hook(Decimal, lambda v, _: Decimal(str(v / 100)))
+converter.register_structure_hook(date, lambda v, _: datetime.strptime(v, '%Y-%m-%d').date())
+
+
+def get_bill_data(bills_data, year_month):
+    return next(b for b in bills_data if b['summary']['due_date'].startswith(year_month))
+
+
+def data_to_rows(bill_detail_data, card_statements_data, nubank, year_month):
+    bill = converter.structure(bill_detail_data['bill'], Bill)
+    return bill.to_rows(card_statements_data, nubank, year_month)
+
+
+@define
+class Entry:
+    amount: Decimal
+    title: str
+    post_date: date
+    charges: int = 1
+    transaction_id: Optional[str] = None
+
+    def find_statement(self, statements_data):
+        logger.info(f'Finding statement for transaction {self.transaction_id}')
+        return next(s for s in statements_data if s['id'] == self.transaction_id)
+
+    def get_details(self, statements_data, nubank):
+        statement = self.find_statement(statements_data)
+        logger.info(f'Requesting card details for statement {statement["id"]}')
+        return nubank.get_card_statement_details(statement)
+
+    def get_installments(self, statements_data, nubank, year_month):
+        details = self.get_details(statements_data, nubank)
+        charges = details['transaction']['charges_list']
+        return [c for c in charges if c['post_date'].startswith(year_month)]
+
+    def get_installment_index(self, statements_data, nubank, year_month):
+        if not self.transaction_id:
+            return 'x'
+
+        installments = self.get_installments(statements_data, nubank, year_month)
+
+        if not installments:
+            return 'x'
+
+        return installments[0]['index']
+
+    def get_installment(self, statements_data, nubank, year_month):
+        if self.charges == 1:
+            return
+
+        installment_index = self.get_installment_index(statements_data, nubank, year_month)
+        return f'{installment_index}/{self.charges}'
+
+    def to_row(self, statements_data, nubank, year_month):
+        return Row(
+            date_=str(self.post_date),
+            description=self.title,
+            bank='Nubank Lucas',
+            business_raw=self.title,
+            installment=self.get_installment(statements_data, nubank, year_month),
+            value=str(-self.amount).replace('.', ','),
+        )
+
+
+@define
+class Bill:
+    line_items: List[Entry]
+
+    def to_rows(self, card_statements_data, nubank, year_month):
+        return [e.to_row(card_statements_data, nubank, year_month) for e in self.line_items]
