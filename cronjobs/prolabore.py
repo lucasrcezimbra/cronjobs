24736b9bf77e8a629ab1a943252b55a42cf21d98
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-05-15 19:07:57 -0300

    feat(prolabore): add logs

--- cronjobs/prolabore.py
+++ cronjobs/prolabore.py
@@ -2,24 +2,30 @@ import sys
 
 from agilize import Agilize, Competence
 from decouple import config
+from loguru import logger
 
 from cronjobs.storage.google import drive
 
 
 def main(competence):
+    logger.info("Starting...")
     agilize = Agilize(
         username=config("AGILIZE_USERNAME"), password=config("AGILIZE_PASSWORD")
     )
+    logger.info("Loading Agilize Company")
     company = agilize.companies()[0]
 
+    logger.info("Loading Prolabore")
     competence = Competence.from_data(competence)
     prolabore = company.prolabores.get(competence)
 
+    logger.info("Uploading Prolabore PDF")
     drive.upload(
         file=prolabore.download(),
         filename=f"{competence}-Lucas Rangel Cezimbra.pdf",
         folder=drive.PROLABORE_FOLDER,
     )
+    logger.success("Finish")
 
 
 if __name__ == "__main__":
